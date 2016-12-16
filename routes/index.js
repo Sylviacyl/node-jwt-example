0a41e3d2fd174efb92930ac30cf94a4984569b91
Sylvia Lee <sylvialee@Sylvias-MacBook-Pro.local>
2016-12-16 14:31:19 +1100

    added routes for special header

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/special', function(req, res, next) {
+ const token = req.query.token;
+ const payLoad=jwt.verify(token,process.env.TOKEN_SECRET);
+ res.json({
+   special: true,
+   email: payLoad.email
+ });
+});
+
+router.get('/special-header', function(req, res, next) {
+ const token = req.header('Authorisation');
+ const token = Authorisation.split(' ')[1];
+ //const token = Authorisation.match('')[];
+ const payLoad=jwt.verify(token,process.env.TOKEN_SECRET);
+ res.json({
+   special: true,
+   email: payLoad.email
+ });
+});
+
 module.exports = router;
